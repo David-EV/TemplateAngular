ba4374e59202e9c94782abaffb43268eb8ddab61
David Enriquez <davidenriquez72@hotmail.com>
2022-05-15 20:27:56 -0500

    conexion a firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,28 @@ export class InfoPaginaService {
 
   info:InfoPagina={};
   cargada=false;
+  equipo: any[]=[];
 
   constructor(private http:HttpClient) {
     console.log("Servicos de la pagina creados");
+    this.cargarInfo();
+    this.cargarEquipo();
 
-    //leer json
-this.http.get('assets/data/data-pagina.json')
-.subscribe( (resp: InfoPagina) => {
-  this.cargada=true;
-  this.info=resp;
-  console.log( resp );
-});
+  }
+  private cargarInfo(){
+ //leer json
+ this.http.get('assets/data/data-pagina.json')
+ .subscribe( (resp: InfoPagina) => {
+   this.cargada=true;
+   this.info=resp;
+ });
+  }
+  private cargarEquipo(){
+    //leer datos equipo
+ this.http.get('https://angular-template-871a5-default-rtdb.firebaseio.com/equipo.json')
+ .subscribe( (resp: any) => {
+   this.equipo=resp;
+   console.log( resp );
+ });
   }
 }
